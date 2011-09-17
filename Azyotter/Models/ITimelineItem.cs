20a40421cdd96ad32f4940a223ada9a0c0f58a80
azyobuzin <azyobuzin@users.sourceforge.jp>
2011-09-17 23:40:50 +0900

    ITimelineItemを用意（ほかは未実装）

---
+++ Azyotter/Models/ITimelineItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Azyobuzi.Azyotter.Models
+{
+    public interface ITimelineItem
+        : IEquatable<ITimelineItem>, INotifyPropertyChanged
+    {
+        string Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        string Text { get; set; }
+        TwitterDataModel.User From { get; set; }
+        TwitterDataModel.User To { get; set; }
+        TwitterDataModel.Status InReplyToStatus { get; set; }//必ず取得してくる
+        TwitterDataModel.Source Source { get; set; }
+    }
+}
